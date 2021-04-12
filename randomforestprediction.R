b76c0890561c7642823915c23e270c33f3f13f48
chlpasin <78547711+chlpasin@users.noreply.github.com>
2021-04-12 16:44:33 +0200

    Add files via upload

--- randomforestprediction.R
+++ randomforestprediction.R
@@ -1,12 +1,13 @@
+library(ranger)
 ###################### Necessary inputs: random forest model + probability threshold + newdata
-load("ABCORA2.2.RFranger.RData")
-mx1_rf<-0.387874
+load("ABCORA2.3.RFranger.RData")
+mx1_rf<-0.3845776
 
 ###################### newdata has to be of the following format
 # each line correspond to a patient
-# columns have to be in the following order: 
+# columns should included the following measurements: 
 # IgG S1, IgG S2, IgG RBD, IgG N, IgA S1, IgA S2, IgA RBD, IgA N, IgM S1, IgM S2, IgM RBD, IgM N
-# names of the columns should be "IgG_S1_LFS", ... "IgG_NP_LFS", ... 
+# with names: "IgG_S1_LFS", "IgG_S2_LFS", ..., "IgM_NP_LFS"
 # no other columns
 # measures of the Igs are log10(Fold over empty)
 
@@ -17,7 +18,6 @@ predict_rf_proba <- as.vector(pred_rf$predictions[,2])
 ##### THIS LINE DETERMINES IF A SAMPLE IS SEROPOSITIVE OR NOT (the proba of being positive has to 
 ##### be higher than mx1_rf)
 prediction_rf <- as.factor(ifelse(predict_rf_proba>mx1_rf,"Positive","Negative"))
-
 
 #### prediction_rf can be added to the dataset
 newdata$serostatus<-prediction_rf
